e759c0555c0d33c9ae3b67fb25e611ee3a25070a
Ismail <ismailtikhlal74@gmail.com>
2024-02-19 12:24:13 +0200

    New

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the length of a rectangle : ");
+        int length = in.nextInt();
+        System.out.println("Enter the breadth of a rectangle : ");
+        int breadth = in.nextInt();
+        int area = length*breadth;
+        System.out.println("The area of a rectangle is : "+area );
+
+
+
+
+          }
+    }
+
